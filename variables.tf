f210e5f79069aadaa0910c7afd21ef55665e3698
Sven Veit <133926521+svenveit@users.noreply.github.com>
2026-07-31 18:15:38 +0200

    Add variables for Azure Terraform configuration

--- variables.tf
+++ variables.tf
@@ -1,1 +1,19 @@
+variable "location" {
+  description = "Azure region for the bootstrap resources."
+  type        = string
+}
 
+variable "resource_group_name" {
+  description = "Name of the resource group for the Terraform backend."
+  type        = string
+}
+
+variable "storage_account_name" {
+  description = "Name of the Storage Account used for the Terraform remote state."
+  type        = string
+}
+
+variable "container_name" {
+  description = "Name of the Blob container for the Terraform state."
+  type        = string
+}
